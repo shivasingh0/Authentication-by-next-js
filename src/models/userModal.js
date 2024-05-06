3a1ecf2914273e7650e73a50f2ef3f709a1aeb71
shivasingh0 <shiva2003rajawat@gmail.com>
2024-05-07 01:35:30 +0530

    setting up with backend

---
+++ src/models/userModal.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide a name"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide an email"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a name"],
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  forgotPasswordToken: String,
+  forgotPasswordTokenExpiry: Date,
+  verfiedToken: String,
+  verfiedTokenExpiry: Date,
+});
+
+const User = mongoose.modal("User", userSchema);
+
+export default User;
